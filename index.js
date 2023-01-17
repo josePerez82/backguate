cfb7543135fa71878020525f1c49cffdc7388224
josePerez82 <joseperezleon1852@hotmail.com>
2023-01-16 20:35:25 -0600

    arreglando error de user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const Port = process.env.Port
 const cors = require('cors');
+const user = require('./routes/routes')
 
 
 const app = express();
